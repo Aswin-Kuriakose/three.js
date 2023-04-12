c2df23ab97649dd1429959e94b59b2ff6b7f2fae
Aswin-Kuriakose <112945895+Aswin-Kuriakose@users.noreply.github.com>
2023-04-12 23:07:56 +0530

    moon around object completed

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
+const parent = new THREE.Object3D()
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/ window.innerHeight, 0.1, 1000 )
+camera.position.set( 0, 0, 0 )
 
 const renderer = new THREE.WebGL1Renderer({
-
+  // alpha: true,
+  antialias: true,
   canvas: document.querySelector('#bg')
 
 })
@@ -25,7 +29,8 @@ const material = new THREE.MeshStandardMaterial({ color: 0xffff45 })
 
 const mesh = new THREE.Mesh( shape, material )
 
-scene.add( mesh )
+// scene.add( mesh )
+parent.add(mesh)
 
 const pointLight = new THREE.PointLight( 0xffffff )
 pointLight.position.set( 4, 10, 0 )
@@ -55,7 +60,48 @@ function stars() {
 
 }
 
+
 Array(500).fill().forEach( stars )
+
+
+const texture = new THREE.TextureLoader().load('./free-space-textures.jpg')
+
+scene.background = texture
+
+const moonTxt = new THREE.TextureLoader().load('./Moon_texture.jpg')
+const moonSurface = new THREE.TextureLoader().load('./moon-surface.jpg')
+
+const moonShape = new THREE.SphereGeometry( 10, 16, 16 )
+const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTxt, normalMap: moonSurface })
+const moon = new THREE.Mesh( moonShape, moonMaterial )
+
+moon.position.set( 50, 50, 50 )
+// scene.add( moon )
+parent.add(moon)
+
+scene.add(parent)
+
+function moveCamera() {
+
+  const elTop = document.body.getBoundingClientRect().top
+
+     camera.position.x = elTop * -0.01
+     camera.position.y = elTop * -0.01
+     camera.position.z = elTop * -0.01
+
+}
+
+// document.addEventListener( 'scroll', () => {
+
+//   const elTop = document.body.getBoundingClientRect().top
+
+//   camera.position.x = elTop * -0.01
+//   camera.position.y = elTop * -0.01
+//   camera.position.z = elTop * -0.01
+
+// } )
+document.body.onscroll = moveCamera
+
 
 function animate() {
 
@@ -64,9 +110,16 @@ function animate() {
   mesh.rotation.x += 0.01
   mesh.rotation.z += 0.01
 
+  moon.rotation.y += 0.002
+  // moon.rotation.y += 0.02
+
+  parent.rotateY(0.01)
+
   controls.update()
   renderer.render( scene, camera )
 
 }
+
+
 
 animate()
